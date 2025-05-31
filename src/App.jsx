bf00d7703092eb30e965ddf362ddbb36e4c66490
lokeshloki3 <lokesh1992p@gmail.com>
2025-05-31 23:46:27 +0530

    list created with checked field

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
+import React, { useState } from "react";
+import { list } from "./list";
 
 function App() {
 
+  const [listItems, setListItems] = useState(
+    list.map((item) => ({ ...item, checked: false }))
+  );
 
+  console.log("listItems", listItems);
+  
   return (
-    <div>
-      Hello
+    <div className="flex justify-center items-center">
+      {
+        listItems.map((item)=> (
+          <div key={item.id}>{item.name}</div>
+        ))
+      }
     </div>
   )
 }
